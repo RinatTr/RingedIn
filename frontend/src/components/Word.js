d04432c0ec5392328c393d6035fdacef51e0e501
RinatTr <rinatreg@gmail.com>
2019-09-17 12:49:37 -0400

    init word display styling

--- frontend/src/components/Word.js
+++ frontend/src/components/Word.js
@@ -3,8 +3,12 @@ const Word = ( { currDisplay } ) => {
   return (
     <ul>
       {currDisplay.map((letter,i) => {
-        return <li key={i}> {letter} <span>_</span> </li>
-      })}
+        return <li key={i}>
+                  <span id="letter">{letter}</span>
+                  <span id="underscore">_</span>
+                </li>
+              })
+        }
     </ul>
   )
 }
