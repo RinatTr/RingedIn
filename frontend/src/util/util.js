ae247df3e0fa017b9afb50deeceab6e115ecd5eb
RinatTr <rinatreg@gmail.com>
2019-09-17 12:33:01 -0400

    add processGuess function

--- frontend/src/util/util.js
+++ frontend/src/util/util.js
@@ -8,3 +8,24 @@ export const getRandomWord = (words) => {
   let randomIdx = Math.floor(Math.random() * words.length);
   return words[randomIdx]
 }
+
+export const isInvalidInput = (input) => {
+  let validChars = "abcdefghijklmnopqrstuvwxyz".split('');
+  return (!validChars.includes(input) || input.length > 1)
+}
+
+export const processGuess = (userInput, currWord, currDisplay) => {
+  let newDisplay, nullCount = 0, isWrongGuess;
+  newDisplay = currWord.map((letter, i) => {
+      if (letter === userInput) {
+        return letter;
+      } else if (currDisplay[i]) {
+        return currDisplay[i];
+      } else {
+        nullCount++;
+        return null;
+      }
+    })
+  isWrongGuess = (nullCount === currWord.length)
+  return { isWrongGuess, newDisplay }
+}
