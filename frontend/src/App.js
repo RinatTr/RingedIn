2dbef935e6268abb48661f34c5545909a1e2d9e8
RinatTr <rinatreg@gmail.com>
2019-09-18 17:55:19 -0400

    update directions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>RingedIn</h1>
+        <h4>Guess the next letter in the word and keep your rings colorful!</h4>
       </header>
       <Main />
     </div>
