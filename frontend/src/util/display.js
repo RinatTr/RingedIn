097e0f2316776f530729721843aad5119200669c
RinatTr <rinatreg@gmail.com>
2019-09-19 15:08:47 -0400

    add result to color function

--- frontend/src/util/display.js
+++ frontend/src/util/display.js
@@ -1,3 +1,11 @@
 export const caseToColor = (id, count) => {
   return (count >= id) ? "grey" : id;
 }
+
+export const resultToColor = (result) => {
+  if (result.isEnd) {
+    return { "color": result.isWin ? "#2A9D8F" : "#BC0000" };
+  } else {
+    return {};
+  }
+}
